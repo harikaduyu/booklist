e436f79b7bb833912c743a9d062756893e241b78
Harika Duyu <h.duyu@rebuy.com>
2020-05-26 20:45:34 +0200

    validation and alert

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function Book(title,author,isbn){
 // UI Constructor
 function UI() {}
 
-//Add book to list
+// Add book to list
 UI.prototype.addBookToList = function(book){
   const list = document.getElementById('book-list');
   //Create tr element
@@ -25,6 +25,28 @@ UI.prototype.addBookToList = function(book){
 
 }
 
+// Show alert 
+UI.prototype.showAlert = function(message,className){
+  // Create div
+  const div = document.createElement('div');
+  // Add classes
+  div.className = `alert ${className}`;
+  // Add text
+  div.appendChild(document.createTextNode(message));
+  // Get parent
+  const container = document.querySelector('.container');
+  // Get form
+  const form = document.querySelector('#book-form');
+  // Insert alert (div) before form 
+  container.insertBefore(div,form);
+
+  // Timeout after 3 sec
+  setTimeout(function(){
+    document.querySelector('.alert').remove();
+  },3000);
+}
+
+// Clear fields
 UI.prototype.clearFields = function(){
   document.getElementById('title').value = '';
   document.getElementById('author').value = '';
@@ -46,12 +68,22 @@ function(e){
   // Istantiate UI
   const ui = new UI();
 
+  // Validate
+  if(title == '' || author == '' || isbn == ''){
+    // Error alert
+    ui.showAlert('Please fill in all fields','error');
+  } else {
+
   // Add book to list
   ui.addBookToList(book);
+
+  // Show success 
+  ui.showAlert("Book added!","success");
 
   //Clear fields
   ui.clearFields();
 
+  }
 
   e.preventDefault();
 });
